e2aaa8e35649b4a0931bd6fdbeca3b83844f431d
root <root@LUK00.internal.ebworld>
2019-09-03 15:51:40 +0200

    Changed class documentation

--- src/Task.h
+++ src/Task.h
@@ -1,10 +1,17 @@
+/*
+********************************************************
+Task.h
+********************************************************
+*/
 #ifndef __TASK__
 #define __TASK__
 
-class Task {
-  public:
-    virtual void init() = 0;
-    virtual void tick() = 0;
-		};
+namespace Tusk {
+    class Task {
+    public:
+        virtual void init() = 0;
+        virtual void tick() = 0;
+    };
+}
 
 #endif
