fd3df56f12f69232e2850e8865fab05a6935f429
Luka Micheletti <michelettiluka@gmail.com>
2019-09-04 18:38:13 +0200

    Added doc to headers

--- src/Task.h
+++ src/Task.h
@@ -1,15 +1,26 @@
 /*
-********************************************************
+***********************************************************
 Task.h
-********************************************************
+
+This class defines the default interface for a Task object.
+Tusk compliance is achieved inheriting from Task.
+
+Copyright (C) 2019 Luka Micheletti
+***********************************************************
 */
+
 #ifndef __TASK__
 #define __TASK__
 
 namespace Tusk {
     class Task {
     public:
+        // Initializes all variables needed by the task.
+        // The init method can be as long and complex as needed, since it is run just once.
         virtual void init() = 0;
+
+        // Executes an atomic action needed by the task to do its job.
+        // The action needs to be as simple and short as possible, since it is executed continuously.
         virtual void tick() = 0;
     };
 }
--- src/Timer.h
+++ src/Timer.h
@@ -1,12 +1,24 @@
+/*
+**************************************************************************
+Timer.h
+
+TODO
+
+Copyright (C) 2019 Luka Micheletti
+**************************************************************************
+*/
+
 #ifndef __TIMER__
 #define __TIMER__
 
-class Timer {
-  public:
-    Timer();
-    void setupFreq(int freq);
-    void setupPeriod(int period);
-    void waitForNextTick();
-};
+namespace Tusk {
+    class Timer {
+    public:
+        Timer();
+        void setupFreq(int freq);
+        void setupPeriod(int period);
+        void waitForNextTick();
+    };
+}
 
 #endif
