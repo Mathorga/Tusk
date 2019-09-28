572878af1ca6e91ca3435be73fe0fcea5f5e63ef
Luka Micheletti <michelettiluka@gmail.com>
2019-09-28 22:37:24 +0200

    Namespaced Timer

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -6,76 +6,78 @@ ISR (TIMER1_COMPA_vect) {
     timerFlag = true;
 }
 
-Timer::Timer(){
-    timerFlag = false;
-}
+namespace Tusk {
+    Timer::Timer(){
+        timerFlag = false;
+    }
 
-void Timer::setupFreq(int frequency) {
-    // Disable interrupts.
-    cli();
+    void Timer::setupFreq(int frequency) {
+        // Disable interrupts.
+        cli();
 
-    // Set entire TCCR1A register to 0.
-    TCCR1A = 0;
+        // Set entire TCCR1A register to 0.
+        TCCR1A = 0;
 
-    // Same for TCCR1B.
-    TCCR1B = 0;
+        // Same for TCCR1B.
+        TCCR1B = 0;
 
-    // Initialize counter value to 0.
-    TCNT1 = 0;
+        // Initialize counter value to 0.
+        TCNT1 = 0;
 
-    // Set compare match register.
-    // OCR1A = (16 * 2 ^ 20) / (100 * PRESCALER) - 1 (must be < 65536)
-    // Assuming a prescaler of 1024 => OCR1A = (16 * 2 ^ 10) / frequency.
-    OCR1A = 16 * 1024 / frequency;
+        // Set compare match register.
+        // OCR1A = (16 * 2 ^ 20) / (100 * PRESCALER) - 1 (must be < 65536)
+        // Assuming a prescaler of 1024 => OCR1A = (16 * 2 ^ 10) / frequency.
+        OCR1A = 16 * 1024 / frequency;
 
-    // Set CTC mode.
-    TCCR1B |= (1 << WGM12);
+        // Set CTC mode.
+        TCCR1B |= (1 << WGM12);
 
-    // Set CS11 for 8 prescaler.
-    TCCR1B |= (1 << CS12) | (1 << CS10);
+        // Set CS11 for 8 prescaler.
+        TCCR1B |= (1 << CS12) | (1 << CS10);
 
-    // Enable timer compare interrupt.
-    TIMSK1 |= (1 << OCIE1A);
+        // Enable timer compare interrupt.
+        TIMSK1 |= (1 << OCIE1A);
 
-    // Enable interrupts.
-    sei();
-}
+        // Enable interrupts.
+        sei();
+    }
 
-void Timer::setupPeriod(int period) {
-    // Disable interrupts.
-    cli();
+    void Timer::setupPeriod(int period) {
+        // Disable interrupts.
+        cli();
 
-    // Set entire TCCR1A register to 0.
-    TCCR1A = 0;
+        // Set entire TCCR1A register to 0.
+        TCCR1A = 0;
 
-    // Same for TCCR1B.
-    TCCR1B = 0;
+        // Same for TCCR1B.
+        TCCR1B = 0;
 
-    // Initialize counter value to 0.
-    TCNT1 = 0;
+        // Initialize counter value to 0.
+        TCNT1 = 0;
 
-    // Set compare match register.
-    // OCR1A = (16 * 2 ^ 20) / (100 * PRESCALER) - 1 (must be < 65536)
-    // Assuming a prescaler of 1024 => OCR1A = (16 * 2 ^ 10) * period / 1000 (being in ms).
-    OCR1A = 16.384 * period;
+        // Set compare match register.
+        // OCR1A = (16 * 2 ^ 20) / (100 * PRESCALER) - 1 (must be < 65536)
+        // Assuming a prescaler of 1024 => OCR1A = (16 * 2 ^ 10) * period / 1000 (being in ms).
+        OCR1A = 16.384 * period;
 
-    // Set CTC mode.
-    TCCR1B |= (1 << WGM12);
+        // Set CTC mode.
+        TCCR1B |= (1 << WGM12);
 
-    // Set CS11 for 8 prescaler.
-    TCCR1B |= (1 << CS12) | (1 << CS10);
+        // Set CS11 for 8 prescaler.
+        TCCR1B |= (1 << CS12) | (1 << CS10);
 
-    // Enable timer compare interrupt.
-    TIMSK1 |= (1 << OCIE1A);
+        // Enable timer compare interrupt.
+        TIMSK1 |= (1 << OCIE1A);
 
-    // Enable interrupts.
-    sei();
-}
+        // Enable interrupts.
+        sei();
+    }
 
-void Timer::waitForNextTick(){
-    // Wait for timer signal.
-    while (!timerFlag) {}
+    void Timer::waitForNextTick(){
+        // Wait for timer signal.
+        while (!timerFlag) {}
 
-    // Reset flag.
-    timerFlag = false;
+        // Reset flag.
+        timerFlag = false;
+    }
 }
